bcd8c1337a437e58dd2e606a7f5aa7b512f72281
TaranKachru <tarankachru@gmail.com>
2026-08-02 17:18:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+const express = require("express");
 
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Home page
+app.get("/", (req, res) => {
+    res.send("Hello from Google Cloud Run!");
+});
+
+// About page
+app.get("/about", (req, res) => {
+    res.send("This is my first Google Cloud Run project.");
+});
+
+// Health check
+app.get("/health", (req, res) => {
+    res.json({
+        status: "Running",
+        message: "Everything is working!"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
